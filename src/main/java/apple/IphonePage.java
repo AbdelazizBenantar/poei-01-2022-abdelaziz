3ac1d780f22a42d57729e64ff4c0522de5d76418
Abdelaziz <abdelaziz.benantar@gmail.com>
2022-01-10 13:55:47 +0100

    Added IphonePageTest

--- src/main/java/apple/IphonePage.java
+++ src/main/java/apple/IphonePage.java
@@ -1,12 +1,26 @@
 package apple;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class IphonePage {
     WebDriver driver;
-
+    By iphone13ProFigure = By.cssSelector(".chapternav-item-iphone-13-pro");
+    By buyIphone13Pro = By.className("ac-ln-button");
+    int timeoutAppearIphone13ProPage = 10;
         public IphonePage(WebDriver driver){
             this.driver = driver;
-
         }
+
+    public void openIphone13ProPage(){
+        driver.findElement(iphone13ProFigure).click();
+        // attendre que l'action soit finie
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutAppearIphone13ProPage));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(buyIphone13Pro));
+
+    }
 }
--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -1,10 +1,15 @@
+import apple.IphonePage;
 import apple.MainPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class AppleTest {
     WebDriver driver;
@@ -26,8 +31,14 @@ public class AppleTest {
         // Arrange
 
         //Act
+
         MainPage mainPage = new MainPage(driver);
         mainPage.openIphonePage();
+
+        IphonePage iphonePage = new IphonePage(driver);
+        iphonePage.openIphone13ProPage();
+
+
 
 
         //Assert
